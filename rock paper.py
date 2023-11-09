9ddf72eda079b768b4dc1479b3d44054b535228e
xdStefcho <84671344+xdStefcho@users.noreply.github.com>
2023-11-09 10:52:40 +0200

    Update rock paper.py

--- rock paper.py
+++ rock paper.py
@@ -36,7 +36,73 @@ while player_wins < wining_score and computer_wins < wining_score:
             player_wins += 1
         else:
             print('Computer wins!')
-            computer_wins += 1
+            computer_wins += 1from random import randint
+
+def reset_scores():
+    return 0, 0
+
+def play_game():
+    player_wins, computer_wins = reset_scores()
+    winning_score = 3
+    while True:
+        if player_wins >= winning_score or computer_wins >= winning_score:
+            break
+        print(f'Player score: {player_wins} Computer score: {computer_wins}')
+        print("..rock..")
+        print("..paper..")
+        print("..scissors")
+
+        player = input("Enter your choice: ").lower()
+        if player == 'quit' or player == 'q':
+            continue
+        random_num = randint(0, 2)
+        if random_num == 0:
+            computer = 'rock'
+        elif random_num == 1:
+            computer = 'paper'
+        else:
+            computer = 'scissors'
+        print(f"The computer plays: {computer}")
+
+        if player == computer:
+            print('It\'s a tie ')
+        elif player == 'rock':
+            if computer == 'scissors':
+                print('Player wins!')
+                player_wins += 1
+            else:
+                print('Computer wins!')
+                computer_wins += 1
+        elif player == 'paper':
+            if computer == 'rock':
+                print('Player wins!')
+                player_wins += 1
+            else:
+                print('Computer wins!')
+                computer_wins += 1
+        elif player == 'scissors':
+            if computer == 'rock':
+                print('Computer wins')
+                computer_wins += 1
+            else:
+                print('Player wins!')
+                player_wins += 1
+        else:
+            print('Enter a valid move!')
+
+    if player_wins > computer_wins:
+        print(f'CONGRATS YOU WON: {player_wins} wins')
+    elif player_wins == computer_wins:
+        print('THE GAME IS DRAW')
+    else:
+        print(f'UNFORTUNATELY COMPUTER WON: {computer_wins} wins')
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again == 'yes':
+        play_game()
+
+play_game()
+
     elif computer == 'rock':
         if player == 'scissors':
             print('Computer wins')
